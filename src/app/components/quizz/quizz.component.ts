e10f087976d9e3ce1385ed0b92dce7492e75dd88
Brunop27 <brunopinheiro715@gmail.com>
2024-01-29 10:55:09 -0300

    Adicionando regras ao quiz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 @Component({
   selector: 'app-quizz',
   standalone: true,
@@ -14,15 +14,42 @@ export class QuizzComponent implements OnInit {
   
   questions:any;
   questionSelected: any;
-
+  
   answers:string[] = [];
   answerSelected:string = "";
-
+  
   questionIndex:number = 0;
   questionMaxIndex:number = 0;
 
   finished:boolean = false;
 
   constructor(){}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+    }
+  }
+
+  buttonChoose(alias:string) {
+    this.answers.push(alias)
+  }
+
+  async nextStep(){
+    this.questionIndex+=1;
+    
+    if(this.questionMaxIndex>this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      this.finished = true
+    }
+  }
 }
