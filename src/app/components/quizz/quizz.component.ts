05bcecf4cd23f62d40e7abc78009d4f31859638d
Brunop27 <brunopinheiro715@gmail.com>
2024-01-29 10:27:17 -0300

    Estruturando parte lógica

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
 
+export class QuizzComponent implements OnInit {
+  title:string = "";
+  
+  questions:any;
+  questionSelected: any;
+
+  answers:string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  constructor(){}
+  ngOnInit(): void {}
 }
